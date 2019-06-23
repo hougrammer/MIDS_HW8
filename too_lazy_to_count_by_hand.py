832b66b8d634b54e9f707b1b44c38b4338c3bc6f
David Hou <david.m.hou@gmail.com>
2019-06-22 21:25:48 -0700

    fix indentation

--- too_lazy_to_count_by_hand.py
+++ too_lazy_to_count_by_hand.py
@@ -3,13 +3,13 @@ import xml.etree.ElementTree as ET
 
 total = millenium_falcon = tie_fighter = 0
 for f in os.listdir('labeled'):
-	if f[-4:] != '.xml': continue
+    if f[-4:] != '.xml': continue
 
-	total += 1
-	root = ET.parse('labeled/' + f).getroot()
-	for tag in root.findall('object/name'):
-		if tag.text == 'millenium falcon': millenium_falcon += 1
-		else: tie_fighter += 1
+    total += 1
+    root = ET.parse('labeled/' + f).getroot()
+    for tag in root.findall('object/name'):
+        if tag.text == 'millenium falcon': millenium_falcon += 1
+        else: tie_fighter += 1
 
 print('total: {}'.format(total))
 print('millenium_falcon: {}'.format(millenium_falcon))
